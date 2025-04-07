391e3de91dd31b4bdc19c29d76443d418e68b3d6
odilitime <janesmith@airmail.cc>
2025-04-07 22:46:11 +0000

    add missing levels

--- packages/core-plugin-v2/src/logger.ts
+++ packages/core-plugin-v2/src/logger.ts
@@ -1,16 +1,30 @@
 import { logger as coreLogger } from '@elizaos/core';
 
 // logger wrapper/specification
-const logger: Record<'trace' | 'debug' | 'info' | 'warn' | 'error' | 'fatal' | 'clear', LogMethod> =
-  {
-    trace: (...args) => coreLogger.trace(...args),
-    debug: (...args) => coreLogger.debug(...args),
-    info: (...args) => coreLogger.info(...args),
-    warn: (...args) => coreLogger.warn(...args),
-    error: (...args) => coreLogger.error(...args),
-    fatal: (...args) => coreLogger.fatal(...args),
-    clear: () => coreLogger.clear(),
-  };
+const logger: Record<
+  | 'trace'
+  | 'debug'
+  | 'success'
+  | 'progress'
+  | 'log'
+  | 'info'
+  | 'warn'
+  | 'error'
+  | 'fatal'
+  | 'clear',
+  LogMethod
+> = {
+  trace: (...args) => coreLogger.trace(...args),
+  debug: (...args) => coreLogger.debug(...args),
+  success: (...args) => coreLogger.debug(...args),
+  progress: (...args) => coreLogger.debug(...args),
+  log: (...args) => coreLogger.info(...args),
+  info: (...args) => coreLogger.info(...args),
+  warn: (...args) => coreLogger.warn(...args),
+  error: (...args) => coreLogger.error(...args),
+  fatal: (...args) => coreLogger.fatal(...args),
+  clear: () => coreLogger.clear(),
+};
 
 export { logger };
 
